83ac2c226197f3f77e64b56a26e4dbc18a075729
paulgreen920-del <paulgreen920@gmail.com>
2025-12-12 21:04:32 -0500

    Remove old Facebook Pixel code from <head>; use Next.js <Script> after <body> instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Link from "next/link";
 
 import Navigation from "@/components/Navigation";
+import Script from "next/script";
 import FacebookPixel from "@/components/FacebookPixel";
 
 export const metadata = {
@@ -15,25 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Facebook Pixel Code */}
-        <script dangerouslySetInnerHTML={{ __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '918220014962169');
-          fbq('track', 'PageView');
-        `}} />
-        <noscript>
-          <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=918220014962169&ev=PageView&noscript=1" />
-        </noscript>
       </head>
       <body className="bg-gray-50 text-gray-900">
-        <FacebookPixel />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          <head>
+          </head>
         {/* NAVBAR */}
         <header className="sticky top-0 z-40 bg-white/80 backdrop-blur border-b">
           <nav className="max-w-6xl mx-auto px-4 h-14 flex items-center justify-between">
