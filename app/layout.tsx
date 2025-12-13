89388ad67744fcb79471c33d531dfadc7c35eb4b
paulgreen920-del <paulgreen920@gmail.com>
2025-12-12 20:43:17 -0500

    Add Facebook Pixel tracking code to global layout (Pixel ID: 918220014962169)

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,24 @@ export default function RootLayout({
 }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Facebook Pixel Code */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '918220014962169');
+          fbq('track', 'PageView');
+        `}} />
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=918220014962169&ev=PageView&noscript=1" />
+        </noscript>
+      </head>
       <body className="bg-gray-50 text-gray-900">
         {/* NAVBAR */}
         <header className="sticky top-0 z-40 bg-white/80 backdrop-blur border-b">
